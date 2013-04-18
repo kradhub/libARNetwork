ebac997474f7312a3f23073dd9123930e4e21cae
Nicolas Brûlez <nicolas.brulez@parrot.com>
2013-04-18 14:37:17 +0200

    Added ARNETWORK_Error_ToString() function

--- Includes/libARNetwork/ARNETWORK_Error.h
+++ Includes/libARNetwork/ARNETWORK_Error.h
@@ -3,39 +3,47 @@
  *  @brief libARNetwork errors known.
  *  @date 05/18/2012
  *  @author maxime.maitre@parrot.com
-**/
+ */
 
 #ifndef _ARNETWORK_ERROR_H_
 #define _ARNETWORK_ERROR_H_
 
 /**
  *  @brief libARNetwork errors known.
-**/
+ */
 typedef enum
 {
-    ARNETWORK_OK = 0, /**< no error */
-    ARNETWORK_ERROR = -1000, /**< error unknown */ 
-    ARNETWORK_ERROR_ALLOC, /**< allocation error */
-    ARNETWORK_ERROR_BAD_PARAMETER, /**< parameter incorrect */ 
-    ARNETWORK_ERROR_ID_UNKNOWN, /**< IOBuffer identifier unknown */ 
-    ARNETWORK_ERROR_BUFFER_SIZE, /**< free space of the buffer is insufficient */
-    ARNETWORK_ERROR_BUFFER_EMPTY, /**< try to read a buffer empty */
-    ARNETWORK_ERROR_SEMAPHORE, /**< error during the using of a semaphore */
-    ARNETWORK_ERROR_MANAGER = -2000, /**< manager error unknown */ 
-    ARNETWORK_ERROR_MANAGER_NEW_IOBUFFER, /**< error during the IOBuffer creation */ 
-    ARNETWORK_ERROR_MANAGER_NEW_SENDER, /**< error during the Sender creation */ 
-    ARNETWORK_ERROR_MANAGER_NEW_RECEIVER, /**< error during the Receiver creation */
-    ARNETWORK_ERROR_NEW_BUFFER, /**< error during the buffer creation */
-    ARNETWORK_ERROR_NEW_RINGBUFFER, /**< error during the RingBuffer creation */
-    ARNETWORK_ERROR_IOBUFFER = -3000, /**< IOBuffer error unknown */ 
-    ARNETWORK_ERROR_IOBUFFER_BAD_ACK,  /**< the sequence number isn't same as that waiting */
-    ARNETWORK_ERROR_SOCKET = -4000,  /**< socket error unknown */
-    ARNETWORK_ERROR_SOCKET_PERMISSION_DENIED, /**< Permission denied */
-    ARNETWORK_ERROR_RECEIVER = -5000, /**< receiver error unknown */
-    ARNETWORK_ERROR_RECEIVER_BUFFER_END, /**< end of the Receiver Buffer */
-    ARNETWORK_ERROR_RECEIVER_BAD_FRAME, /**< error bad frame */
-    
+    ARNETWORK_OK = 0, /**< No error */
+    ARNETWORK_ERROR = -1000, /**< Unknown generic error */
+    ARNETWORK_ERROR_ALLOC, /**< Memory allocation error */
+    ARNETWORK_ERROR_BAD_PARAMETER, /**< Bad parameters */
+    ARNETWORK_ERROR_ID_UNKNOWN, /**< Given IOBuffer identifier is unknown */
+    ARNETWORK_ERROR_BUFFER_SIZE, /**< Insufficient free space in the buffer */
+    ARNETWORK_ERROR_BUFFER_EMPTY, /**< Buffer is empty, nothing was read */
+    ARNETWORK_ERROR_SEMAPHORE, /**< Error when using a semaphore */
+    ARNETWORK_ERROR_MANAGER = -2000, /**< Unknown ARNETWORK_Manager error */
+    ARNETWORK_ERROR_MANAGER_NEW_IOBUFFER, /**< IOBuffer creation error */
+    ARNETWORK_ERROR_MANAGER_NEW_SENDER, /**< Sender creation error */
+    ARNETWORK_ERROR_MANAGER_NEW_RECEIVER, /**< Receiver creation error */
+    ARNETWORK_ERROR_NEW_BUFFER, /**< Buffer creation error */
+    ARNETWORK_ERROR_NEW_RINGBUFFER, /**< RingBuffer creation error */
+    ARNETWORK_ERROR_IOBUFFER = -3000, /**< Unknown IOBuffer error */
+    ARNETWORK_ERROR_IOBUFFER_BAD_ACK,  /**< Bad sequence number for the acknowledge */
+    ARNETWORK_ERROR_SOCKET = -4000,  /**< Unknown socket error */
+    ARNETWORK_ERROR_SOCKET_PERMISSION_DENIED, /**< Permission denied on a socket */
+    ARNETWORK_ERROR_RECEIVER = -5000, /**< Unknown Receiver error */
+    ARNETWORK_ERROR_RECEIVER_BUFFER_END, /**< Receiver buffer too small */
+    ARNETWORK_ERROR_RECEIVER_BAD_FRAME, /**< Bad frame content on network */
+
 } eARNETWORK_ERROR;
 
-#endif /** _ARNETWORK_ERROR_H_ */
+/**
+ * @brief Gets the error string associated with an eARNETWORK_ERROR
+ * @param error The error to describe
+ * @return A static string describing the error
+ *
+ * @note User should NEVER try to modify a returned string
+ */
+const char *const ARNETWORK_Error_ToString (eARNETWORK_ERROR error);
 
+#endif /* _ARNETWORK_ERROR_H_ */
---
+++ Sources/ARNETWORK_Error.c
@@ -0,0 +1,94 @@
+/**
+ * @file ARNETWORK_Error.c
+ * @brief informations about libARNetwork errors
+ * @date 04/18/2013
+ * @author nicolas.brulez@parrot.com
+ */
+
+#include <libARNetwork/ARNETWORK_Error.h>
+
+//
+// To generate the function using emacs regexp syntax
+// - remove all cases (except for default)
+// - copy the content (no braces) of eARNETWORK_ERROR enum
+// - replace rexexp (M-x replace-regexp) with the following parameters
+//   - Source: [\ \t]*\([A-Z_]*\)[-0-9\ \t,=]*/\*\*<\ \(.*\)\ \*/
+//   - Dest  : case \1:^Jreturn "\2";^J;break;
+// - Reindent all lines in file
+//
+// Note: ^J character is the newline character, C-q C-j in emacs
+//
+
+const char *const ARNETWORK_Error_ToString (eARNETWORK_ERROR error)
+{
+    switch (error)
+    {
+    case ARNETWORK_OK:
+        return "No error";
+        break;
+    case ARNETWORK_ERROR:
+        return "Unknown generic error";
+        break;
+    case ARNETWORK_ERROR_ALLOC:
+        return "Memory allocation error";
+        break;
+    case ARNETWORK_ERROR_BAD_PARAMETER:
+        return "Bad parameters";
+        break;
+    case ARNETWORK_ERROR_ID_UNKNOWN:
+        return "Given IOBuffer identifier is unknown";
+        break;
+    case ARNETWORK_ERROR_BUFFER_SIZE:
+        return "Insufficient free space in the buffer";
+        break;
+    case ARNETWORK_ERROR_BUFFER_EMPTY:
+        return "Buffer is empty, nothing was read";
+        break;
+    case ARNETWORK_ERROR_SEMAPHORE:
+        return "Error when using a semaphore";
+        break;
+    case ARNETWORK_ERROR_MANAGER:
+        return "Unknown ARNETWORK_Manager error";
+        break;
+    case ARNETWORK_ERROR_MANAGER_NEW_IOBUFFER:
+        return "IOBuffer creation error";
+        break;
+    case ARNETWORK_ERROR_MANAGER_NEW_SENDER:
+        return "Sender creation error";
+        break;
+    case ARNETWORK_ERROR_MANAGER_NEW_RECEIVER:
+        return "Receiver creation error";
+        break;
+    case ARNETWORK_ERROR_NEW_BUFFER:
+        return "Buffer creation error";
+        break;
+    case ARNETWORK_ERROR_NEW_RINGBUFFER:
+        return "RingBuffer creation error";
+        break;
+    case ARNETWORK_ERROR_IOBUFFER:
+        return "Unknown IOBuffer error";
+        break;
+    case ARNETWORK_ERROR_IOBUFFER_BAD_ACK:
+        return "Bad sequence number for the acknowledge";
+        break;
+    case ARNETWORK_ERROR_SOCKET:
+        return "Unknown socket error";
+        break;
+    case ARNETWORK_ERROR_SOCKET_PERMISSION_DENIED:
+        return "Permission denied on a socket";
+        break;
+    case ARNETWORK_ERROR_RECEIVER:
+        return "Unknown Receiver error";
+        break;
+    case ARNETWORK_ERROR_RECEIVER_BUFFER_END:
+        return "Receiver buffer too small";
+        break;
+    case ARNETWORK_ERROR_RECEIVER_BAD_FRAME:
+        return "Bad frame content on network";
+        break;
+    default:
+        return "Unknown error";
+        break;
+    }
+    return "Unknown error";
+}
