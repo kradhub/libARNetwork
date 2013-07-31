e307a78e1a0573e2f34e7a05bfbb794b2ba17db5
Nicolas Brûlez <nicolas.brulez@parrot.com>
2013-07-31 16:52:12 +0200

    Fixed for ARBuildUtils/Android

--- JNI/c/Android.mk
+++ JNI/c/Android.mk
@@ -1,22 +1,36 @@
 LOCAL_PATH := $(call my-dir)
 
+# libARSAL
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := LIBARSAL-prebuilt
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libarsal.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+# libNetworkAL
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := LIBARNETWORKAL-prebuilt
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libarnetworkal.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
 # libNetwork
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := LIBARNETWORK-prebuilt
-LOCAL_SRC_FILES := lib/libarnetwork.a
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libarnetwork.a
 
 include $(PREBUILT_STATIC_LIBRARY)
 
 # WRAPPER_LIB
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES:= $(LOCAL_PATH)/include $(LOCAL_PATH)/../libARSAL/include $(LOCAL_PATH)/../libARNetworkAL/include
+LOCAL_C_INCLUDES:= $(LOCAL_PATH)/include
 LOCAL_LDLIBS := -llog
 LOCAL_MODULE := libarnetwork_android
 LOCAL_SRC_FILES := ARNETWORK_JNIManager.c  ARNETWORK_JNIIOBufferParam.c
-LOCAL_CFLAGS := -O0 -g
-LOCAL_STATIC_LIBRARIES := LIBARNETWORK-prebuilt
-LOCAL_SHARED_LIBRARIES := LIBARSAL-prebuilt LIBARNETWORKAL-prebuilt
+LOCAL_STATIC_LIBRARIES := LIBARNETWORK-prebuilt LIBARSAL-prebuilt LIBARNETWORKAL-prebuilt
 include $(BUILD_SHARED_LIBRARY)
 
