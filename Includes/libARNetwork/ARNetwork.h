b9ec0201724f4408bfb711bfcc3f98a5a76b3e20
Nicolas Brûlez <nicolas.brulez@parrot.com>
2013-12-26 14:47:05 +0100

    Added a generic ARNetwork.h header which includes all ARNetwork headers

---
+++ Includes/libARNetwork/ARNetwork.h
@@ -0,0 +1,15 @@
+/**
+ * @file ARNetwork.h
+ * @brief Global header for libARNetwork
+ * @date 12/04/2013
+ * @author nicolas.brulez@parrot.com
+ */
+
+#ifndef _ARNETWORK_H_
+#define _ARNETWORK_H_
+
+#include <libARNetwork/ARNETWORK_Error.h>
+#include <libARNetwork/ARNETWORK_IOBufferParam.h>
+#include <libARNetwork/ARNETWORK_Manager.h>
+
+#endif /* _ARNETWORK_H_ */
